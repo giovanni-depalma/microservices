c22c06bd9f23f138d68f9412005a192be4a8cd24
giovanni.depalma <giovanni.depalma@softeco.it>
2020-07-16 13:01:34 +0200

    gateway consul

---
+++ spring-gateway-consul/src/main/java/com/reactgenova/springmicroservice/rest/Fallback.java
@@ -0,0 +1,15 @@
+package com.reactgenova.springmicroservice.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Mono;
+
+@RestController
+public class Fallback {
+
+    @RequestMapping("/fallback")
+    public Mono<String> fallback() {
+        return Mono.just("fallback");
+    }
+}
